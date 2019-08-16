24a557d50e95db4151279c680dc1fc7645357937
Emre Can HACIOSMANOĞLU <emrecan.haciosmanoglu@agu.edu.tr>
2019-08-16 14:49:20 +0300

    Assertion

--- Can/Can/src/Can/Core.h
+++ Can/Can/src/Can/Core.h
@@ -1,13 +1,21 @@
 #pragma once
 
 #ifdef CAN_PLATFORM_WINDOWS
-	#ifdef CAN_BUILD_DLL
-		#define CAN_API __declspec(dllexport)
-	#else
-		#define CAN_API __declspec(dllimport)
-	#endif // CAN_BUILD_DLL
+#ifdef CAN_BUILD_DLL
+#define CAN_API __declspec(dllexport)
 #else
-	#error CAN only supports Windows!
+#define CAN_API __declspec(dllimport)
+#endif // CAN_BUILD_DLL
+#else
+#error CAN only supports Windows!
 #endif // CAN_PLATFORM_WINDOWS
+
+#ifdef CAN_ENABLE_ASSERTS
+#define CAN_ASSERT(x, ...) {if(!(x)) { CAN_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak();}}
+#define CAN_CORE_ASSERT(x, ...) {if(!(x)) { CAN_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak();}}
+#else
+#define CAN_ASSERT(x, ...)
+#define CAN_CORE_ASSERT(x, ...)
+#endif
 
 #define BIT(x) (1 << x)
