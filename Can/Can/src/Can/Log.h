2e6bd923da0085586727d60093d2035b5573ee63
Emre Can HACIOSMANOĞLU <emrecan.haciosmanoglu@agu.edu.tr>
2019-11-10 18:28:34 +0300

    fixed wrong spdlog api call

--- Can/Can/src/Can/Log.h
+++ Can/Can/src/Can/Log.h
@@ -25,11 +25,11 @@ namespace Can
 #define CAN_CORE_INFO(...)  ::Can::Log::GetCoreLogger()->info(__VA_ARGS__)
 #define CAN_CORE_WARN(...)  ::Can::Log::GetCoreLogger()->warn(__VA_ARGS__)
 #define CAN_CORE_ERROR(...) ::Can::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define CAN_CORE_FATAL(...) ::Can::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+#define CAN_CORE_FATAL(...) ::Can::Log::GetCoreLogger()->critical(__VA_ARGS__)
 
 //Client log macros
 #define CAN_TRACE(...)      ::Can::Log::GetClientLogger()->trace(__VA_ARGS__)
 #define CAN_INFO(...)       ::Can::Log::GetClientLogger()->info(__VA_ARGS__)
 #define CAN_WARN(...)       ::Can::Log::GetClientLogger()->warn(__VA_ARGS__)
 #define CAN_ERROR(...)      ::Can::Log::GetClientLogger()->error(__VA_ARGS__)
-#define CAN_FATAL(...)      ::Can::Log::GetClientLogger()->fatal(__VA_ARGS__)
+#define CAN_FATAL(...)      ::Can::Log::GetClientLogger()->critical(__VA_ARGS__)
