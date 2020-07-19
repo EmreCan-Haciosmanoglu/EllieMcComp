978ff108fe0f3026ce72a71ba69a0fe3a387f669
Emre Can HACIOSMANOĞLU <emrecan.haciosmanoglu@agu.edu.tr>
2020-07-19 23:58:05 +0300

    Custom iterators are added

--- Can/Can/src/Can/Scene/ChildrenComponent.h
+++ Can/Can/src/Can/Scene/ChildrenComponent.h
@@ -15,6 +15,14 @@ namespace Can
 		entt::entity& operator[](size_t index) { return Children[index]; }
 		const entt::entity& operator[](size_t index) const { return Children[index]; }
 
+		std::vector<entt::entity>::iterator begin() { return Children.begin(); }
+		std::vector<entt::entity>::const_iterator begin() const { return Children.begin(); }
+		std::vector<entt::entity>::const_iterator cbegin() const { return Children.cbegin(); }
+		
+		std::vector<entt::entity>::iterator end() { return Children.end(); }
+		std::vector<entt::entity>::const_iterator end() const { return Children.end(); }
+		std::vector<entt::entity>::const_iterator cend() const { return Children.cend(); }
+
 		std::vector<entt::entity> Children;
 	};
 }
