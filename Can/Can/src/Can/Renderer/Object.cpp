bfa0a50c3840d190d74d703c775e49f575dda86f
Emre Can HACIOSMANOĞLU <emrecan.haciosmanoglu@agu.edu.tr>
2020-08-10 16:31:17 +0300

    Refactoring

--- Can/Can/src/Can/Renderer/Object.cpp
+++ Can/Can/src/Can/Renderer/Object.cpp
@@ -1,43 +1,55 @@
 #include "canpch.h"
 #include "Object.h"
+#include "Can.h"
 
 namespace Can
 {
-	Object::Object(const Ref<Prefab>& prefab, const Ref<Prefab>& type, bool enabled)
+	Object::Object(Prefab* prefab, Prefab* type, bool enabled)
 		: prefab(prefab)
 		, type(type)
 		, position({ 0.0f, 0.0f, 0.0f })
 		, scale({ 1.0f, 1.0f, 1.0f })
-		, transform(glm::mat4(1.0f))
+		, rotation({ 0.0f, 0.0f, 0.0f })
+		, q(glm::quat(rotation))
+		, transform(glm::translate(glm::mat4(1.0f), position) * glm::mat4_cast(q)* glm::scale(glm::mat4(1.0f), scale))
+		, enabled(enabled)
 	{
+		Renderer3D::AddObject(this);
 	}
-	Object::Object(const Ref<Prefab>& prefab, const Ref<Prefab>& type, const glm::mat4& transform, bool enabled)
+	Object::Object(Prefab* prefab, Prefab* type, const glm::mat4& transform, bool enabled)
 		: prefab(prefab)
 		, type(type)
 		, transform(transform)
 		, enabled(enabled)
 	{
 		glm::decompose(transform, scale, q, position, skew, perspective);
+		rotation = glm::eulerAngles(q);
+		Renderer3D::AddObject(this);
 	}
-	Object::Object(const Ref<Prefab>& prefab, const Ref<Prefab>& type, const glm::vec3& position, bool enabled)
+	Object::Object(Prefab* prefab, Prefab* type, const glm::vec3& position, bool enabled)
 		: prefab(prefab)
 		, type(type)
 		, position(position)
 		, scale({ 1.0f, 1.0f, 1.0f })
-		, transform(glm::translate(glm::mat4(1.0f), position))
+		, rotation({ 0.0f, 0.0f, 0.0f })
+		, q(glm::quat(rotation))
+		, transform(glm::translate(glm::mat4(1.0f), position)* glm::mat4_cast(q)* glm::scale(glm::mat4(1.0f), scale))
 		, enabled(enabled)
 	{
+		Renderer3D::AddObject(this);
 	}
-	Object::Object(const Ref<Prefab>& prefab, const Ref<Prefab>& type, const glm::vec3& position, const glm::vec3& scale, bool enabled)
+	Object::Object(Prefab* prefab, Prefab* type, const glm::vec3& position, const glm::vec3& scale, bool enabled)
 		: prefab(prefab)
 		, type(type)
 		, position(position)
 		, scale(scale)
-		, transform(glm::translate(glm::mat4(1.0f), position)* glm::scale(glm::mat4(1.0f), scale))
+		, rotation({ 0.0f, 0.0f, 0.0f })
+		, q(glm::quat(rotation))
+		, transform(glm::translate(glm::mat4(1.0f), position)* glm::mat4_cast(q)* glm::scale(glm::mat4(1.0f), scale))
 		, enabled(enabled)
 	{
 	}
-	Object::Object(const Ref<Prefab>& prefab, const Ref<Prefab>& type, const glm::vec3& position, const glm::vec3& scale, const glm::vec3& rotation, bool enabled)
+	Object::Object(Prefab* prefab, Prefab* type, const glm::vec3& position, const glm::vec3& scale, const glm::vec3& rotation, bool enabled)
 		: prefab(prefab)
 		, type(type)
 		, position(position)
@@ -47,36 +59,43 @@ namespace Can
 		, transform(glm::translate(glm::mat4(1.0f), position)* glm::mat4_cast(q)* glm::scale(glm::mat4(1.0f), scale))
 		, enabled(enabled)
 	{
+		Renderer3D::AddObject(this);
 	}
-	Object::Object(const Ref<Prefab>& prefab, const Ref<Prefab>& type, const glm::vec3& position, const glm::vec3& scale, const glm::quat& q, bool enabled)
+	Object::Object(Prefab* prefab, Prefab* type, const glm::vec3& position, const glm::vec3& scale, const glm::quat& q, bool enabled)
 		: prefab(prefab)
 		, type(type)
 		, position(position)
 		, scale(scale)
-		, rotation({ 0.0f, 0.0f, 0.0f })
+		, rotation(glm::eulerAngles(q))
 		, q(q)
 		, transform(glm::translate(glm::mat4(1.0f), position)* glm::mat4_cast(q)* glm::scale(glm::mat4(1.0f), scale))
 		, enabled(enabled)
 	{
-		rotation = glm::eulerAngles(q);
+		Renderer3D::AddObject(this);
+	}
+	Object::~Object()
+	{
+		Renderer3D::DeleteObject(this);
 	}
 	void Object::SetTransform(const glm::mat4& transform)
 	{
 		this->transform = transform;
 		glm::decompose(transform, scale, q, position, skew, perspective);
+		SetTransform(this->position, this->scale, this->rotation);
+
 	}
 	void Object::SetTransform(const glm::vec3& position)
 	{
 		this->position = position;
 
-		this->transform = glm::translate(glm::mat4(1.0f), position) * glm::mat4_cast(q) * glm::scale(glm::mat4(1.0f), scale);
+		SetTransform(this->position, this->scale, this->rotation);
 	}
 	void Object::SetTransform(const glm::vec3& position, const glm::vec3& scale)
 	{
 		this->position = position;
 		this->scale = scale;
 
-		this->transform = glm::translate(glm::mat4(1.0f), position) * glm::mat4_cast(q) * glm::scale(glm::mat4(1.0f), scale);
+		SetTransform(this->position, this->scale, this->rotation);
 	}
 	void Object::SetTransform(const glm::vec3& position, const glm::vec3& scale, const glm::vec3& rotation)
 	{
--- Can/Can/src/Can/Renderer/Object.h
+++ Can/Can/src/Can/Renderer/Object.h
@@ -1,19 +1,20 @@
 #pragma once
-#include "Can.h"
+#include "Can/Renderer/Prefab.h"
+#include <glm/glm.hpp>
 
 namespace Can
 {
 	class Object
 	{
 	public:
-		Object(const Ref<Prefab>& prefab, const Ref<Prefab>& type, bool enabled = true);
-		Object(const Ref<Prefab>& prefab, const Ref<Prefab>& type, const glm::mat4& transform, bool enabled = true);
-		Object(const Ref<Prefab>& prefab, const Ref<Prefab>& type, const glm::vec3& position, bool enabled = true);
-		Object(const Ref<Prefab>& prefab, const Ref<Prefab>& type, const glm::vec3& position, const glm::vec3& scale, bool enabled = true);
-		Object(const Ref<Prefab>& prefab, const Ref<Prefab>& type, const glm::vec3& position, const glm::vec3& scale, const glm::vec3& rotation, bool enabled = true);
-		Object(const Ref<Prefab>& prefab, const Ref<Prefab>& type, const glm::vec3& position, const glm::vec3& scale, const glm::quat& q, bool enabled = true);
+		Object(Prefab* prefab, Prefab* type, bool enabled = true);
+		Object(Prefab* prefab, Prefab* type, const glm::mat4& transform, bool enabled = true);
+		Object(Prefab* prefab, Prefab* type, const glm::vec3& position, bool enabled = true);
+		Object(Prefab* prefab, Prefab* type, const glm::vec3& position, const glm::vec3& scale, bool enabled = true);
+		Object(Prefab* prefab, Prefab* type, const glm::vec3& position, const glm::vec3& scale, const glm::vec3& rotation, bool enabled = true);
+		Object(Prefab* prefab, Prefab* type, const glm::vec3& position, const glm::vec3& scale, const glm::quat& q, bool enabled = true);
 		
-		~Object() = default;
+		~Object();
 
 		void SetTransform(const glm::mat4& transform);
 		void SetTransform(const glm::vec3& position);
@@ -21,8 +22,8 @@ namespace Can
 		void SetTransform(const glm::vec3& position, const glm::vec3& scale, const glm::vec3& rotation);
 
 	public:
-		Ref<Prefab> prefab;
-		Ref<Prefab> type;
+		Prefab* prefab;
+		Prefab* type;
 
 		glm::vec3 position;
 		glm::vec3 scale;
