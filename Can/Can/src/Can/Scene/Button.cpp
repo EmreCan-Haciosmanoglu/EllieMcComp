8767e4484408e3c56f983db9953e3158f01d1969
Emre Can HACIOSMANOĞLU <emrecan.haciosmanoglu@agu.edu.tr>
2020-08-31 18:23:43 +0300

    Button class's constructors are combined into one that takes parameter struct

--- Can/Can/src/Can/Scene/Button.cpp
+++ Can/Can/src/Can/Scene/Button.cpp
@@ -4,67 +4,18 @@
 
 namespace Can
 {
-	Button::Button(
-		entt::registry& registry,
-		entt::entity parent
-	)
-		: sceneRegistry(&registry)
-		, entityID(sceneRegistry->create())
+	Button::Button(ButtonConstructorParameters parameters)
+		: sceneRegistry(parameters.Registry)
+		, entityID(sceneRegistry.create())
 	{
-		registry.emplace<ParentComponent>(entityID, parent);
-		sceneRegistry->emplace<TransformComponent>(entityID);
-		sceneRegistry->emplace<SpriteRendererComponent>(entityID);
-	}
-	Button::Button(
-		entt::registry& registry,
-		entt::entity parent,
-		const glm::vec3& position,
-		const glm::vec3& size,
-		const glm::vec4& color
-	)
-		: sceneRegistry(&registry)
-		, entityID(sceneRegistry->create())
-	{
-		registry.emplace<ParentComponent>(entityID, parent);
-		sceneRegistry->emplace<TransformComponent>(entityID, position);
-		sceneRegistry->emplace<SpriteRendererComponent>(entityID, size, color);
-	}
-	Button::Button(
-		entt::registry& registry,
-		entt::entity parent,
-		const glm::vec3& position,
-		const glm::vec3& size,
-		const glm::vec4& color,
-		std::function<void()> onClick
-	)
-		: sceneRegistry(&registry)
-		, entityID(sceneRegistry->create())
-	{
-		registry.emplace<ParentComponent>(entityID, parent);
-		sceneRegistry->emplace<TransformComponent>(entityID, position);
-		sceneRegistry->emplace<SpriteRendererComponent>(entityID, size, color);
-		sceneRegistry->emplace<OnClickCallbackComponent>(entityID, onClick);
-	}
-	Button::Button(entt::registry& registry)
-		: sceneRegistry(&registry)
-		, entityID(sceneRegistry->create())
-	{
-		sceneRegistry->emplace<TransformComponent>(entityID);
-		sceneRegistry->emplace<SpriteRendererComponent>(entityID);
-	}
-	Button::Button(entt::registry& registry, const glm::vec3& position, const glm::vec3& size, const glm::vec4& color)
-		: sceneRegistry(&registry)
-		, entityID(sceneRegistry->create())
-	{
-		sceneRegistry->emplace<TransformComponent>(entityID, position);
-		sceneRegistry->emplace<SpriteRendererComponent>(entityID, size, color);
-	}
-	Button::Button(entt::registry& registry, const glm::vec3& position, const glm::vec3& size, const glm::vec4& color, std::function<void()> onClick)
-		: sceneRegistry(&registry)
-		, entityID(sceneRegistry->create())
-	{
-		sceneRegistry->emplace<TransformComponent>(entityID, position);
-		sceneRegistry->emplace<SpriteRendererComponent>(entityID, size, color);
-		sceneRegistry->emplace<OnClickCallbackComponent>(entityID, onClick);
+		if (parameters.Parent != entt::null)
+			sceneRegistry.emplace<ParentComponent>(entityID, parameters.Parent);
+
+		sceneRegistry.emplace<TransformComponent>(entityID, parameters.Position);
+		sceneRegistry.emplace<SpriteRendererComponent>(entityID, parameters.Size, parameters.Texture, parameters.Color);
+
+		if (parameters.OnClick)
+			sceneRegistry.emplace<OnClickCallbackComponent>(entityID, parameters.OnClick);
+
 	}
 }
--- Can/Can/src/Can/Scene/Button.h
+++ Can/Can/src/Can/Scene/Button.h
@@ -3,48 +3,23 @@
 
 namespace Can
 {
+	struct ButtonConstructorParameters
+	{
+		entt::registry& Registry;
+		entt::entity Parent = entt::null;
+		const glm::vec3& Position = glm::vec3(0.0f);
+		const glm::vec3& Size = glm::vec3(1.0f);
+		const glm::vec4& Color = glm::vec4(1.0f);
+		const Ref<Texture2D>& Texture = nullptr;
+		std::function<void()> OnClick;
+	};
 	class Button
 	{
 	public:
-		Button(
-			entt::registry& registry,
-			entt::entity parent
-		);
-		Button(
-			entt::registry& registry,
-			entt::entity parent,
-			const glm::vec3& position,
-			const glm::vec3& size,
-			const glm::vec4& color
-		);
-		Button(
-			entt::registry& registry,
-			entt::entity parent,
-			const glm::vec3& position,
-			const glm::vec3& size,
-			const glm::vec4& color,
-			std::function<void()> onClick
-		);
-		Button(
-			entt::registry& registry
-		);
-
-		Button(
-			entt::registry& registry,
-			const glm::vec3& position,
-			const glm::vec3& size,
-			const glm::vec4& color
-		);
-		Button(
-			entt::registry& registry,
-			const glm::vec3& position,
-			const glm::vec3& size,
-			const glm::vec4& color,
-			std::function<void()> onClick
-		);
+		Button(ButtonConstructorParameters parameters);
 
 	public:
-		entt::registry* sceneRegistry;
+		entt::registry& sceneRegistry;
 		entt::entity entityID;
 	};
 }
