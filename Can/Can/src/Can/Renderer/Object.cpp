470ece1490b61cea174716d7ba43f1e3ac67fd01
Emre Can HACIOSMANOĞLU <emrecan.haciosmanoglu@agu.edu.tr>
2020-09-14 13:10:14 +0300

    Rotation priority is added

--- Can/Can/src/Can/Renderer/Object.cpp
+++ Can/Can/src/Can/Renderer/Object.cpp
@@ -101,6 +101,11 @@ namespace Can
 		this->rotation = rotation;
 		this->q = glm::quat(this->rotation);
 
-		this->transform = glm::translate(glm::mat4(1.0f), position) * glm::mat4_cast(q) * glm::scale(glm::mat4(1.0f), scale);
+		this->transform = 
+			glm::translate(glm::mat4(1.0f), position) * 
+			glm::rotate(glm::mat4(1.0f), rotation.y, glm::vec3{ 0.0f, 1.0f, 0.0f }) *
+			glm::rotate(glm::mat4(1.0f), rotation.z, glm::vec3{ 0.0f, 0.0f, 1.0f }) *
+			glm::rotate(glm::mat4(1.0f), rotation.x, glm::vec3{ 1.0f, 0.0f, 0.0f }) *
+			glm::scale(glm::mat4(1.0f), scale);
 	}
 }
