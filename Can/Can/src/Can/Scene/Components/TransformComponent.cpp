eb3f57470bf7bddf869f196b4ec4e8b1511fbe93
Emre Can HACIOSMANOĞLU <emrecan.haciosmanoglu@agu.edu.tr>
2021-02-03 17:14:47 +0300

    TransformComponent is rewritten

--- Can/Can/src/Can/Scene/Components/TransformComponent.cpp
+++ Can/Can/src/Can/Scene/Components/TransformComponent.cpp
@@ -3,24 +3,19 @@
 
 namespace Can
 {
-	TransformComponent::TransformComponent()
-		: Transform(glm::mat4(1))
-	{
-	}
 	TransformComponent::TransformComponent(const glm::vec3& position)
-		: Transform(glm::translate(glm::mat4(1),position))
+		: Position(position)
 	{
 	}
-	TransformComponent::TransformComponent(const glm::vec3& position, const glm::vec3& scale)
-		: Transform(glm::translate(glm::mat4(1), position) *glm::scale(glm::mat4(1),scale))
+	TransformComponent::TransformComponent(const glm::vec3& position, const glm::vec2& scale)
+		: Position(position)
+		, Scale(scale)
 	{
 	}
-	TransformComponent::TransformComponent(const glm::vec3& position, const glm::vec3& scale, float rotationInRadians)
-		: Transform(glm::translate(glm::mat4(1), position) * glm::rotate(glm::mat4(1.0f), rotationInRadians, glm::vec3(0, 0, 1)) * glm::scale(glm::mat4(1), scale))
-	{
-	}
-	TransformComponent::TransformComponent(const glm::mat4& transform)
-		: Transform(transform)
+	TransformComponent::TransformComponent(const glm::vec3& position, const glm::vec2& scale, float rotationInRadians)
+		: Position(position)
+		, Scale(scale)
+		, Rotation(rotationInRadians)
 	{
 	}
 }
--- Can/Can/src/Can/Scene/Components/TransformComponent.h
+++ Can/Can/src/Can/Scene/Components/TransformComponent.h
@@ -4,16 +4,17 @@ namespace Can
 {
 	struct TransformComponent
 	{
-		TransformComponent();
+		TransformComponent() = default;
 		TransformComponent(const TransformComponent&) = default;
 		TransformComponent(const glm::vec3& position);
-		TransformComponent(const glm::vec3& position, const glm::vec3& scale);
-		TransformComponent(const glm::vec3& position, const glm::vec3& scale, float rotation);
-		TransformComponent(const glm::mat4& transform);
+		TransformComponent(const glm::vec3& position, const glm::vec2& scale);
+		TransformComponent(const glm::vec3& position, const glm::vec2& scale, float rotation);
 
-		operator glm::mat4& () { return Transform; }
-		operator const glm::mat4& () const { return Transform; }
+		operator glm::mat4& () { return glm::translate(glm::mat4(1), Position) * glm::rotate(glm::mat4(1.0f), Rotation, glm::vec3(0, 0, 1)) * glm::scale(glm::mat4(1), glm::vec3(Scale, 1.0f)); }
+		operator const glm::mat4& () const { return glm::translate(glm::mat4(1), Position) * glm::rotate(glm::mat4(1.0f), Rotation, glm::vec3(0.0f, 0.0f, 1.0f)) * glm::scale(glm::mat4(1), glm::vec3(Scale, 1.0f)); }
 
-		glm::mat4 Transform;
+		glm::vec3 Position = glm::vec3(0.0f);
+		glm::vec2 Scale = glm::vec2(1.0f);
+		float Rotation = 0.0f;
 	};
 }
