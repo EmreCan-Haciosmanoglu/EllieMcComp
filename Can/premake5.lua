c91036e378c76e7fe1a8213f26d8671e089393e9
Emre Can HACIOSMANOĞLU <emrecan.haciosmanoglu@agu.edu.tr>
2021-04-23 21:52:58 +0300

    FlappyFish Game is moved to its own repo:https://github.com/EmreCan-Haciosmanoglu/flappy_fish

--- Can/premake5.lua
+++ Can/premake5.lua
@@ -204,56 +204,6 @@ project "2048_Extended"
         runtime "Release"
         optimize "on"
 
-project "FlappyFish"
-    location "FlappyFish"
-    kind "ConsoleApp"
-    language "C++"
-    cppdialect "C++17"
-    staticruntime "On"
-    
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-    
-    files
-    {
-        "%{prj.name}/src/**.h",
-        "%{prj.name}/src/**.cpp",
-        "%{prj.name}/vendor/glm/glm/**.hpp",
-        "%{prj.name}/vendor/glm/glm/**.inl"
-    }
-    
-    includedirs
-    {
-        "Can/vendor/spdlog/include",
-        "Can/src",
-        "%{IncludeDir.imgui}",
-        "%{IncludeDir.glm}"
-    }
-    
-    links
-    {
-        "imgui",
-        "Can"
-    }
-    
-    filter "system:windows"
-        systemversion "latest"
-    
-    filter "configurations:Debug"
-        defines "CAN_DEBUG"
-        runtime "Debug"
-        symbols "on"
-
-    filter "configurations:Release"
-        defines "CAN_RELEASE"
-        runtime "Release"
-        optimize "on"
-    
-    filter "configurations:Dist"
-        defines "CAN_DIST"
-        runtime "Release"
-        optimize "on"
-
 project "Sandbox3D"
     location "Sandbox3D"
     kind "ConsoleApp"
