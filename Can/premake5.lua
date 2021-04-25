c436ab9451917d8369e3c7fc2425264529fa02cf
Emre Can HACIOSMANOĞLU <emrecan.haciosmanoglu@agu.edu.tr>
2021-04-26 01:38:59 +0300

    2048_Extended is moved to its own repo: https://github.com/EmreCan-Haciosmanoglu/2048_Extended

--- Can/premake5.lua
+++ Can/premake5.lua
@@ -156,56 +156,6 @@ project "Sandbox"
         runtime "Release"
         optimize "on"
             
-project "2048_Extended"
-    location "2048_Extended"
-    kind "ConsoleApp"
-    language "C++"
-    cppdialect "C++17"
-    staticruntime "On"
-
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-    files
-    {
-        "%{prj.name}/src/**.h",
-        "%{prj.name}/src/**.cpp",
-        "%{prj.name}/vendor/glm/glm/**.hpp",
-        "%{prj.name}/vendor/glm/glm/**.inl"
-    }
-
-    includedirs
-    {
-        "Can/vendor/spdlog/include",
-        "Can/src",
-        "%{IncludeDir.imgui}",
-        "%{IncludeDir.glm}"
-    }
-
-    links
-    {
-        "imgui",
-        "Can"
-    }
-
-    filter "system:windows"
-        systemversion "latest"
-
-    filter "configurations:Debug"
-        defines "CAN_DEBUG"
-        runtime "Debug"
-        symbols "on"
-
-    filter "configurations:Release"
-        defines "CAN_RELEASE"
-        runtime "Release"
-        optimize "on"
-
-    filter "configurations:Dist"
-        defines "CAN_DIST"
-        runtime "Release"
-        optimize "on"
-
 project "Sandbox3D"
     location "Sandbox3D"
     kind "ConsoleApp"
