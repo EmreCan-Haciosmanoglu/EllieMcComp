a342127239bb81d5bbe96984bd359f71fecafb91
Emre Can HACIOSMANOĞLU <emre.can.haciosmanoglu@hotmail.com>
2021-08-31 23:36:08 +0300

    no more resizable windows

--- Can/Can/src/Platform/Windows/WindowsWindow.cpp
+++ Can/Can/src/Platform/Windows/WindowsWindow.cpp
@@ -65,6 +65,7 @@ namespace Can::Platform::Windows
 			if (Renderer::GetAPI() == RendererAPI::API::OpenGL)
 				glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
 #endif
+			glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 			m_Window = glfwCreateWindow((int)props.Width, (int)props.Height, m_Data.Title.c_str(), nullptr/*glfwGetPrimaryMonitor()*/, nullptr);
 			++s_GLFWWindowCount;
 		}
