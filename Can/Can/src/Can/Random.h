1d86328f659e6e176b0d90d72bb0875b1cfeb744
Emre Can Hacıosmanoğlu <emrecan.haciosmanoglu@agu.edu.tr>
2024-06-19 22:54:04 +0300

    Random::unsigned_8

--- Can/Can/src/Can/Random.h
+++ Can/Can/src/Can/Random.h
@@ -11,6 +11,9 @@ namespace Can::Utility
 		Random(const Random&) = delete;
 		Random& operator=(const Random&) = delete;
 
+		inline static u8 unsigned_8(u8 start, u8 end) { return rand() % (end - start) + start; }
+		inline static u8 unsigned_8(u8 end) { return unsigned_8(0, end); }
+
 		inline static int signed_32(s32 start, s32 end) { return rand() % (end - start) + start; }
 		inline static int signed_32(s32 end) { return signed_32(0, end); }
 
