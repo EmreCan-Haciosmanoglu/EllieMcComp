2266020f980546d922b6969fceb68d99e04c97f5
Emre Can Hacıosmanoğlu <emrecan.haciosmanoglu@agu.edu.tr>
2024-10-05 14:53:46 +0300

    DirectX12 Ep. 31.2

--- Can/Can/src/Can/Graphics/DirectX12/D3D12Core.cpp
+++ Can/Can/src/Can/Graphics/DirectX12/D3D12Core.cpp
@@ -1,5 +1,6 @@
 #include "canpch.h"
 #include "D3D12Core.h"
+#include "D3D12Resources.h"
 
 
 namespace Can::graphics::d3d12::core
@@ -112,9 +113,9 @@ namespace Can::graphics::d3d12::core
 				core::release(_fence);
 				_fence_value = 0;
 
-				CloseHandle(_fence_event); 
+				CloseHandle(_fence_event);
 				_fence_event = nullptr;
-				
+
 
 				core::release(_cmd_queue);
 				core::release(_cmd_list);
@@ -147,12 +148,13 @@ namespace Can::graphics::d3d12::core
 				void release()
 				{
 					core::release(cmd_allocator);
+					fence_value = 0;
 				}
 			};
 
-			ID3D12CommandQueue*         _cmd_queue{ nullptr };
+			ID3D12CommandQueue* _cmd_queue{ nullptr };
 			ID3D12GraphicsCommandList6* _cmd_list{ nullptr };
-			ID3D12Fence1*               _fence;
+			ID3D12Fence1* _fence;
 			u64                         _fence_value{ 0 };
 			HANDLE                      _fence_event{ nullptr };
 			command_frame               _cmd_frames[frame_buffer_count]{};
@@ -161,7 +163,15 @@ namespace Can::graphics::d3d12::core
 
 		ID3D12Device8* main_device{ nullptr };
 		IDXGIFactory7* dxgi_factory{ nullptr };
-		d3d12_command  gfx_command;
+		d3d12_command   gfx_command;
+		descriptor_heap rtv_desc_heap{ D3D12_DESCRIPTOR_HEAP_TYPE_RTV };
+		descriptor_heap dsv_desc_heap{ D3D12_DESCRIPTOR_HEAP_TYPE_DSV };
+		descriptor_heap srv_desc_heap{ D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV };
+		descriptor_heap uav_desc_heap{ D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV };
+
+		std::vector<IUnknown*> deferred_releases[frame_buffer_count]{};
+		u32                    deferred_releases_flag[frame_buffer_count]{};
+		std::mutex             deferred_releases_mutex{};
 
 		constexpr D3D_FEATURE_LEVEL minimum_feature_level{ D3D_FEATURE_LEVEL_11_0 };
 
@@ -211,6 +221,35 @@ namespace Can::graphics::d3d12::core
 		}
 	}
 
+	void __declspec(noinline) process_deferred_releases(u32 frame_idx)
+	{
+		std::lock_guard{ deferred_releases_mutex };
+
+		deferred_releases_flag[frame_idx] = 0;
+		rtv_desc_heap.process_deferred_free(frame_idx);
+		dsv_desc_heap.process_deferred_free(frame_idx);
+		srv_desc_heap.process_deferred_free(frame_idx);
+		uav_desc_heap.process_deferred_free(frame_idx);
+
+		std::vector<IUnknown*> resources{ deferred_releases[frame_idx] };
+		if (!resources.empty())
+		{
+			for (auto& resource : resources) release(resource);
+			resources.clear();
+		}
+	}
+
+	namespace detail
+	{
+		void deferred_release(IUnknown* resource)
+		{
+			const u32 frame_idx{ current_frame_index() };
+			std::lock_guard lock{ deferred_releases_mutex };
+			deferred_releases[frame_idx].push_back(resource);
+			set_deferred_releases_flag();
+		}
+	}
+
 	bool initialize()
 	{
 		if (main_device) shutdown();
@@ -219,8 +258,14 @@ namespace Can::graphics::d3d12::core
 #ifdef _DEBUG
 		{
 			Microsoft::WRL::ComPtr<ID3D12Debug3> debug_interface;
-			DXCall(D3D12GetDebugInterface(IID_PPV_ARGS(&debug_interface)));
+			if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debug_interface))))
+			{
 			debug_interface->EnableDebugLayer();
+			}
+			else
+			{
+				OutputDebugStringA("Warning: D3D12 Debug interface is not available. Verify that Graphics Tools option is installed in this device.\n");
+			}
 			dxgi_factory_flags |= DXGI_CREATE_FACTORY_DEBUG;
 		}
 #endif // _DEBUG
@@ -240,11 +285,6 @@ namespace Can::graphics::d3d12::core
 		DXCall(hr = D3D12CreateDevice(main_adapter.Get(), max_feature_level, IID_PPV_ARGS(&main_device)));
 		if (FAILED(hr)) return failed_init();
 
-		new(&gfx_command) d3d12_command(main_device, D3D12_COMMAND_LIST_TYPE_DIRECT);
-		if (!gfx_command.command_queue()) return failed_init();
-
-		NAME_D3D12_OBJECT(main_device, L"Main D3D12 Device");
-
 #ifdef _DEBUG
 		{
 			Microsoft::WRL::ComPtr<ID3D12InfoQueue> info_queue;
@@ -255,13 +295,40 @@ namespace Can::graphics::d3d12::core
 		}
 #endif // _DEBUG
 
+		bool result{ true };
+		result &= rtv_desc_heap.initialize(512, false);
+		result &= dsv_desc_heap.initialize(512, false);
+		result &= srv_desc_heap.initialize(4096, true);
+		result &= uav_desc_heap.initialize(512, false);
+		if (!result) return failed_init();
+
+		new(&gfx_command) d3d12_command(main_device, D3D12_COMMAND_LIST_TYPE_DIRECT);
+		if (!gfx_command.command_queue()) return failed_init();
+
+		NAME_D3D12_OBJECT(main_device, L"Main D3D12 Device");
+		NAME_D3D12_OBJECT(rtv_desc_heap.heap(), L"RTV Descriptor Heap");
+		NAME_D3D12_OBJECT(dsv_desc_heap.heap(), L"DSV Descriptor Heap");
+		NAME_D3D12_OBJECT(srv_desc_heap.heap(), L"SRV Descriptor Heap");
+		NAME_D3D12_OBJECT(uav_desc_heap.heap(), L"UAV Descriptor Heap");
+
 		return true;
 	}
 
 	void shutdown()
 	{
 		gfx_command.release();
+
+		for (u32 i{ 0 }; i < frame_buffer_count; ++i)
+			process_deferred_releases(i);
+
 		release(dxgi_factory);
+
+		rtv_desc_heap.release();
+		dsv_desc_heap.release();
+		srv_desc_heap.release();
+		uav_desc_heap.release();
+
+		process_deferred_releases(0);
 
 #ifdef _DEBUG
 		{
@@ -291,11 +358,25 @@ namespace Can::graphics::d3d12::core
 
 		ID3D12GraphicsCommandList6* cmd_list{ gfx_command.command_list() };
 
+		const u32 frame_idx{ current_frame_index() };
+		if (deferred_releases_flag[frame_idx])
+		{
+			process_deferred_releases(frame_idx);
+		}
+
 		gfx_command.end_frame();
 	}
 
 	ID3D12Device* const device()
 	{
 		return main_device;
+	}
+	u32 current_frame_index()
+	{
+		return gfx_command.frame_index();
+	}
+	void set_deferred_releases_flag()
+	{
+		deferred_releases_flag[current_frame_index()] = -1;
 	}
 }
--- Can/Can/src/Can/Graphics/DirectX12/D3D12Resources.h
+++ Can/Can/src/Can/Graphics/DirectX12/D3D12Resources.h
@@ -5,6 +5,7 @@
 namespace Can::graphics::d3d12
 {
 	class descriptor_heap;
+
 	struct descriptor_handle
 	{
 		D3D12_CPU_DESCRIPTOR_HANDLE cpu{};
@@ -29,6 +30,7 @@ namespace Can::graphics::d3d12
 		~descriptor_heap() { assert(!_heap); }
 
 		bool initialize(u32 capacity, bool is_shader_visible);
+		void process_deferred_free(u32 frame_idx);
 		void release();
 
 		[[nodiscard]] descriptor_handle allocate();
@@ -48,6 +50,7 @@ namespace Can::graphics::d3d12
 		D3D12_CPU_DESCRIPTOR_HANDLE _cpu_start{};
 		D3D12_GPU_DESCRIPTOR_HANDLE _gpu_start{};
 		std::unique_ptr<u32[]> _free_handles{};
+		std::vector<u32> _deferred_free_indices[frame_buffer_count]{};
 		std::mutex _mutex{};
 		u32 _capacity{ 0 };
 		u32 _size{ 0 };
