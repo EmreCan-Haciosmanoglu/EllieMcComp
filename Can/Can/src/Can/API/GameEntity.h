0bca4a9b145f689162bafcbdd1caee72fe3c271c
Emre Can Hacıosmanoğlu <emrecan.haciosmanoglu@agu.edu.tr>
2024-11-19 21:40:41 +0300

    DirectX12 Ep. 52.1

--- Can/Can/src/Can/API/GameEntity.h
+++ Can/Can/src/Can/API/GameEntity.h
@@ -18,11 +18,16 @@ namespace Can
 		public:
 			constexpr explicit entity(entity_id id) : _id{ id } {}
 			constexpr entity() : _id{ id::invalid_id } {}
-			constexpr entity_id get_id() const { return _id; }
-			constexpr bool is_valid() const { return id::is_valid(_id); }
+			[[nodiscard]] constexpr entity_id get_id() const { return _id; }
+			[[nodiscard]] constexpr bool is_valid() const { return id::is_valid(_id); }
 
-			transform::component transform() const;
-			script::component script() const;
+			[[nodiscard]] transform::component transform() const;
+			[[nodiscard]] script::component script() const;
+
+			[[nodiscard]] math::v4 rotation() const { return transform().rotation(); }
+			[[nodiscard]] math::v3 orientation() const { return transform().orientation(); }
+			[[nodiscard]] math::v3 position() const { return transform().position(); }
+			[[nodiscard]] math::v3 scale() const { return transform().scale(); }
 		private:
 			entity_id _id;
 		};
@@ -38,6 +43,16 @@ namespace Can
 		protected:
 			constexpr explicit entity_script(game_entity::entity entity)
 				:game_entity::entity{ entity.get_id() }{}
+
+			void set_rotation(math::v4 rotation_quaternion) const { set_rotation(this, rotation_quaternion); }
+			void set_orientation(math::v3 orientation_vector) const { set_orientation(this, orientation_vector); }
+			void set_position(math::v3 position) const { set_position(this, position); }
+			void set_scale(math::v3 scale) const { set_scale(this, scale); }
+
+			static void set_rotation(const game_entity::entity* const entity, math::v4 rotation_quaternion);
+			static void set_orientation(const game_entity::entity* const entity, math::v3 orientation_vector);
+			static void set_position(const game_entity::entity* const entity, math::v3 position);
+			static void set_scale(const game_entity::entity* const entity, math::v3 scale);
 		};
 
 		namespace detail
@@ -47,7 +62,7 @@ namespace Can
 			using script_hash = std::hash<std::string>;
 
 			u8 register_script(size_t, script_creator);
-
+			script_creator get_script_creator(size_t tag);
 			template<class script_class>
 			script_ptr create_script(game_entity::entity entity)
 			{
--- Can/Can/src/Can/Components/Script.cpp
+++ Can/Can/src/Can/Components/Script.cpp
@@ -1,16 +1,20 @@
 #include "canpch.h"
 #include "Script.h"
+#include "Transform.h"
 #include "Can/Unordered_Array.h"
 
 namespace Can::script
 {
 	namespace
 	{
-		utl::vector<detail::script_ptr>   entity_scripts;
-		utl::vector<id::id_type>          id_mapping;
+		utl::vector<detail::script_ptr>         entity_scripts;
+		utl::vector<id::id_type>                id_mapping;
 
-		utl::vector<id::generation_type>  generations;
-		utl::deque<script_id>             free_ids;
+		utl::vector<id::generation_type>        generations;
+		utl::deque<script_id>                   free_ids;
+
+		utl::vector<transform::component_cache> transform_cache;
+		std::unordered_map<id::id_type, u32>    cache_map;
 
 		using script_registery = std::unordered_map < size_t, detail::script_creator>;
 
@@ -29,6 +33,31 @@ namespace Can::script
 				entity_scripts[id_mapping[index]] &&
 				entity_scripts[id_mapping[index]]->is_valid();
 		}
+
+		transform::component_cache* const get_cache_ptr(const game_entity::entity* const entity)
+		{
+			assert(game_entity::is_alive(entity->get_id()));
+			const transform::transform_id id{ entity->transform().get_id() };
+
+			u32 index{ u32_invalid_id };
+
+			auto pair = cache_map.try_emplace(id, id::invalid_id);
+
+			if (pair.second)
+			{
+				index = (u32)transform_cache.size();
+				transform_cache.emplace_back();
+				transform_cache.back().id = id;
+				cache_map[id] = index;
+			}
+			else
+			{
+				index = cache_map[id];
+			}
+
+			assert(index < transform_cache.size());
+			return &transform_cache[index];
+		}
 	}
 
 	namespace detail
@@ -38,6 +67,13 @@ namespace Can::script
 			bool result{ registery().insert(script_registery::value_type{tag,func}).second };
 			assert(result);
 			return result;
+		}
+
+		script_creator get_script_creator(size_t tag)
+		{
+			auto script = Can::script::registery().find(tag);
+			assert(script != Can::script::registery().end() && script->first == tag);
+			return script->second;
 		}
 	}
 
@@ -79,6 +115,49 @@ namespace Can::script
 		utl::erase_unordered(entity_scripts, index);
 		id_mapping[id::index(last_id)] = index;
 		id_mapping[id::index(id)] = id::invalid_id;
+	}
 
+	void update(float dt)
+	{
+		for (auto& ptr : entity_scripts)
+		{
+			ptr->update(dt);
+		}
+
+		if (transform_cache.size())
+		{
+			transform::update(transform_cache.data(), (u32)transform_cache.size());
+			transform_cache.clear();
+
+			cache_map.clear();
+		}
+	}
+
+	void entity_script::set_rotation(const game_entity::entity* const entity, math::v4 rotation_quaternion)
+	{
+		transform::component_cache& cache{ *get_cache_ptr(entity) };
+		cache.flags |= transform::component_flags::rotation;
+		cache.rotation = rotation_quaternion;
+	}
+
+	void entity_script::set_orientation(const game_entity::entity* const entity, math::v3 orientation_vector)
+	{
+		transform::component_cache& cache{ *get_cache_ptr(entity) };
+		cache.flags |= transform::component_flags::orientation;
+		cache.orientation = orientation_vector;
+	}
+
+	void entity_script::set_position(const game_entity::entity* const entity, math::v3 position)
+	{
+		transform::component_cache& cache{ *get_cache_ptr(entity) };
+		cache.flags |= transform::component_flags::position;
+		cache.position = position;
+	}
+
+	void entity_script::set_scale(const game_entity::entity* const entity, math::v3 scale)
+	{
+		transform::component_cache& cache{ *get_cache_ptr(entity) };
+		cache.flags |= transform::component_flags::scale;
+		cache.scale = scale;
 	}
 }
